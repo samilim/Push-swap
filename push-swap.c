321d148f78aeed40a73fba01ad98f991d9c651ee
Samantha LIMON <salimon@e2r2p16.42.fr>
2021-05-17 18:17:01 +0200

    debut

--- push-swap.c
+++ push-swap.c
@@ -1,16 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sorting.c                                          :+:      :+:    :+:   */
+/*   push-swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: salimon <salimon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/16 17:57:16 by salimon           #+#    #+#             */
-/*   Updated: 2021/05/17 18:14:37 by salimon          ###   ########.fr       */
+/*   Created: 2021/05/17 17:55:07 by salimon           #+#    #+#             */
+/*   Updated: 2021/05/17 18:12:04 by salimon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push-swap.h"
 
-//plus petit nb au sommet
-//afficer instructions suivies dun \n
+void    push_swap()
+{
+    while (!(sorted()))
+}
+
+int main(char *argv, int argc)
+{
+    if (/*error : param non int ou doublon et si instructions n'existent pas pour bonus*/)
+        write (1, "Error\n", 6);
+}
---
+++ push-swap.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push-swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salimon <salimon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/17 17:55:42 by salimon           #+#    #+#             */
+/*   Updated: 2021/05/17 18:04:00 by salimon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <unistd.h>
+
+typedef struct  s_pile_a
+{
+
+}               t_pile_a;
+
+typedef struct  s_pile_b
+{
+
+}               t_pile_b;
+
+
+#endif
